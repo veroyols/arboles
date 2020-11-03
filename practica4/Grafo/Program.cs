4b5b65a23ab444004f9bef52a89a3866f3f3d5f0
veroyols <vero.scholz@gmail.com>
2020-11-03 05:13:44 -0300

    museo

---
+++ practica4/Grafo/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: veros
+ * Fecha: 3/11/2020
+ * Hora: 03:27
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace tp7
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Grafo <string> museo = new Grafo<string>();
+			
+			Vertice <string> sala1 = new Vertice <string> ("sala1");
+			Vertice <string> sala2 = new Vertice <string> ("sala2");
+			
+			museo.agregarVertice(sala1);
+			museo.agregarVertice(sala2);
+
+			museo.conectar(sala1,sala2,0);
+			museo.conectar(sala2,sala1,0);
+
+			
+			Vertice <string> sala3 = new Vertice <string> ("sala3");
+			Vertice <string> sala4 = new Vertice <string> ("sala4");
+			
+			museo.agregarVertice(sala3);
+			museo.agregarVertice(sala4);
+
+			museo.conectar(sala1,sala3,0);
+			museo.conectar(sala3,sala1,0);
+
+			museo.conectar(sala1,sala4,0);
+			museo.conectar(sala4,sala1,0);
+			
+			museo.conectar(sala2,sala3,0);
+			museo.conectar(sala3,sala2,0);
+			
+			
+			museo.BFS(sala4);
+			//museo.DFS(sala1);
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ practica4/Grafo/cls_Grafo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tp7
+{
+
+	public class Grafo<T>
+	{
+		public Grafo()
+		{
+		}
+		
+		private List<Vertice<T>>vertices = new List<Vertice<T>>();
+	
+		public void agregarVertice(Vertice<T> v) {
+			v.setPosicion(vertices.Count + 1);
+			vertices.Add(v);
+		}
+
+		public void eliminarVertice(Vertice<T> v) {
+			vertices.Remove(v);
+		}
+
+		public void conectar(Vertice<T> origen, Vertice<T> destino, int peso) {
+			origen.getAdyacentes().Add(new Arista<T>(destino,peso));
+		}
+
+		public void desConectar(Vertice<T> origen, Vertice<T> destino) {
+			Arista<T> arista = origen.getAdyacentes().Find(a => a.getDestino().Equals(destino));
+			origen.getAdyacentes().Remove(arista);
+		}
+
+//		public void desConectar(Vertice<T> origen, Vertice<T> destino) {
+//			Arista<T> arista;
+//			foreach (var a in origen.getAdyacentes()) {
+//				if (a.getDestino().Equals(destino))
+//			}		arista = a;
+//			origen.getAdyacentes().Remove(arista);
+//		}		
+	
+		public List<Vertice<T>> getVertices() {
+			return vertices;
+		}
+
+	
+		public Vertice<T> vertice(int indice) {
+			return this.vertices[indice];
+		}
+	
+
+		public void DFS(Vertice<T> origen) {
+			bool [] visitados = new bool [this.vertices.Count];
+			this._DFS (origen, visitados);
+		}
+		
+		private void _DFS(Vertice<T> origen, bool [ ] visitados) {
+			//Imprimo el origen
+			Console.WriteLine (origen.getDato ());
+			//Marcar origen como visitado
+			visitados [origen.getPosicion()-1] = true;
+			//Recorro adyacentes
+			foreach (var adyacente in origen.getAdyacentes()) {
+				//si no fue visitado -> recursiva
+				if (!visitados[adyacente.getDestino().getPosicion()-1])
+					this._DFS(adyacente.getDestino(),visitados);
+			}
+		}
+		
+		public void BFS(Vertice<T> origen) {
+			bool [] visitados = new bool [this.vertices.Count];
+			Queue<Vertice<T>> cola = new Queue<Vertice<T>>();
+			Vertice<T> verticeAux;
+			
+			cola.Enqueue(origen);
+			visitados[origen.getPosicion() - 1] = true;
+			
+			while (cola.Count!=0) {
+				verticeAux = cola.Dequeue();
+				Console.WriteLine(verticeAux.getDato());
+				//recorro adyacentes y encolo no visitados
+				foreach (var adyacentes in verticeAux.getAdyacentes()) { //chequeo si fue visitado
+					if (!visitados[adyacentes.getDestino().getPosicion() - 1]) {
+						cola.Enqueue(adyacentes.getDestino());
+						visitados[adyacentes.getDestino().getPosicion() - 1] = true;
+					}
+				}
+				
+			}
+		}
+		
+		
+	}
+}
